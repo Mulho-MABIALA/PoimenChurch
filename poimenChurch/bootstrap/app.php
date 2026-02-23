2de07a72b2f3ae3286832a9282c28054d6048230
Mulho MABIALA <imulhomabiala@gmail.com>
2026-02-23 00:16:02 +0000

    hello

--- poimenChurch/bootstrap/app.php
+++ poimenChurch/bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -13,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $_exceptions): void {
+    ->withExceptions(function (): void {
         //
     })->create();
 
